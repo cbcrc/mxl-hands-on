98055d786039effff8daf0c145430bb23bb0ed48
rochonma <mathieu.rochon@radio-canada.ca>
2026-05-27 20:59:44 -0400

    fix the starting script in exercise-5 for spx-server setup

--- docker/exercise-5/start.sh
+++ docker/exercise-5/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p /Volumes/mxl/domain_1
 cp ./data/domain_def.json /Volumes/mxl/domain_1
-# To be uncommented when ready to test with the html5 keyer application
-# tar -xf ./data/spx-server/spx-server.tar.xz -C ./data/spx-server --skip-old-files
+tar -xf ./data/spx-server/spx-server.tar.xz -C ./data/spx-server --skip-old-files
 docker compose up -d
 docker container ls
