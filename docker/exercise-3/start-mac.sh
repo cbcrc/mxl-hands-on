f0ca36c83dba83adaa255fb2251577493a31aa44
rochonma <mathieu.rochon@radio-canada.ca>
2026-07-14 15:29:45 -0400

    Fix the start.sh in exercise-3 for mac.

--- docker/exercise-3/start-mac.sh
+++ docker/exercise-3/start-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2026 CBC/Radio-Canada
 # SPDX-License-Identifier: Apache-2.0
-docker compose -f docker-compose.yml -f docker-compose.mac.yml down
+docker compose -f docker-compose.yml -f docker-compose.mac.yml up -d
 docker container ls
